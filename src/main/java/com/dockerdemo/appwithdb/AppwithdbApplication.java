f18f89433d09399934f9f9bed452d74cc6d01460
Terence <terencebc92@gmail.com>
2023-12-14 23:29:31 +0800

    First commit

---
+++ src/main/java/com/dockerdemo/appwithdb/AppwithdbApplication.java
@@ -0,0 +1,13 @@
+package com.dockerdemo.appwithdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppwithdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppwithdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dockerdemo/appwithdb/AppwithdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerdemo.appwithdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppwithdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
